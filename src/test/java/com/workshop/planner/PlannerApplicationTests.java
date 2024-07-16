2f25b49ebbf11559f74c4160da9fd7beca6a82e0
Vinicius Vequeti <viniciusvequeti@gmail.com>
2024-07-16 09:36:57 -0300

    Project created

---
+++ src/test/java/com/workshop/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
